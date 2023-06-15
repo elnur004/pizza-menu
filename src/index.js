0ef0da1716f9d8a53b85c12191ce88b8a8f1fb60
elnur004 <lnur004@gmail.com>
2023-06-15 05:02:48 +0400

    changed short circuiting (&&) operator to the ternary one

--- src/index.js
+++ src/index.js
@@ -70,19 +70,21 @@ const Header = () => {
 
 const Menu = () => {
   const pizzas = pizzaData;
-  // const pizzas = [];
+  // const pizzas = [];  // For checking if there is no any pizza in the menu.
   const pizzasNum = pizzas.length;
 
   return (
     <main className="menu">
       <h2>Our Menu</h2>
 
-      {pizzasNum > 0 && (
+      {pizzasNum > 0 ? (
         <ul className="pizzas">
           {pizzas.map((pizza) => (
             <Pizza pizzaObj={pizza} key={pizza.name} />
           ))}
         </ul>
+      ) : (
+        <p>We're still working on our menu. Please come back later :)</p>
       )}
 
       {/* <Pizza
@@ -119,7 +121,7 @@ const Pizza = (props) => {
 
 const Footer = () => {
   const hour = new Date().getHours();
-  const openHour = 0;
+  const openHour = 10;
   const closeHour = 22;
 
   console.log(hour);
@@ -132,11 +134,15 @@ const Footer = () => {
 
   return (
     <footer className="footer">
-      {isOpen && (
+      {isOpen ? (
         <div className="order">
           <p>We're open until {closeHour}:00. Come visit us or order online.</p>
           <button className="btn">Order</button>
         </div>
+      ) : (
+        <p>
+          We're happy to welcome you between {openHour}:00 and {closeHour}:00.
+        </p>
       )}
     </footer>
   );
